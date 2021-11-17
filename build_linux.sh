42afa05bebe6f98e5028527a88e29dd7dd95ddfd
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2021-11-17 15:39:47 +0100

    update linux build script to be explicit

--- build_linux.sh
+++ build_linux.sh
@@ -3,16 +3,20 @@
 mkdir -p bin
 cd bin
 
-if [ $1 = "release" ]; then
+if [[ $1 = "release" ]]; then
 	echo "Building release mode"
 	mkdir -p release
 	cd release
 	cmake -DCMAKE_BUILD_TYPE=Release ../..
-else
+elif [[ $1 = "debug" ]]; then
 	echo "Building debug mode"
 	mkdir -p debug
 	cd debug
 	cmake -DCMAKE_BUILD_TYPE=Debug ../..
+else
+	echo "Missing argument 1"
+	echo "Usage: sh build_linux.sh [debug/release]"
+	exit 1
 fi
 
 make
